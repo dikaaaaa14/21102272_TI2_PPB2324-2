e6e179a5e09e84751ee10b1e8fdd56d3884b82e0
dikaaaaa14 <147191460+dikaaaaa14@users.noreply.github.com>
2024-06-13 23:37:17 +0700

    Praktikum_06_07

---
+++ praktikum_06_07/praktikum_06_07/lib/pages/profile/profile_page.dart
@@ -0,0 +1,16 @@
+// ignore_for_file: use_super_parameters, avoid_unnecessary_containers, prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+class ProfilePage extends StatelessWidget {
+  const ProfilePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: const Text('Profile'),
+      ),
+    );
+  }
+}
---
+++ praktikum_06_07/praktikum_06_07/lib/pages/support/support_page.dart
@@ -0,0 +1,16 @@
+// ignore_for_file: use_super_parameters, avoid_unnecessary_containers, prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+class SupportPage extends StatelessWidget {
+  const SupportPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: const Text('Support'),
+      ),
+    );
+  }
+}
